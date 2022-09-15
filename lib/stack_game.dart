f2a68a7740034e0565cc088df5537ac703c1e885
Mrhealer <longkmttv9@gmail.com>
2022-09-15 21:28:37 +0700

    update

--- lib/stack_game.dart
+++ lib/stack_game.dart
@@ -5,3 +5,6 @@ const MethodChannel _channel = MethodChannel('stack_game');
 
 /// Scanning Bar Code or QR Code return content
 Future<String?> play() async => await _channel.invokeMethod('play');
+
+/// Scanning Bar Code or QR Code return content
+Future<String?> play2048() async => await _channel.invokeMethod('play2048');
